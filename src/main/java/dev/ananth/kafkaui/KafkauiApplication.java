7d728d50d74adc753b499deb1eeb522e704af4c8
ananth <athraya24@gmail.com>
2023-04-16 17:19:39 +0530

    no change

--- src/main/java/dev/ananth/kafkaui/KafkauiApplication.java
+++ src/main/java/dev/ananth/kafkaui/KafkauiApplication.java
@@ -13,23 +13,6 @@ import java.util.Set;
 @SpringBootApplication
 public class KafkauiApplication {
 
-
-
-	public static void main(String[] args) {
-
-		SpringApplication.run(KafkauiApplication.class, args);
-
-		/*Properties properties = new Properties();
-		properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,"127.0.0.1:9092");
-		properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-		properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-
-		KafkaConsumer<String,String> consumer = new KafkaConsumer<String, String>(properties);*/
-
-
-
-	}
-
-
-
+	public static void main(String[] args) {SpringApplication.run(KafkauiApplication.class, args);}
+	
 }
